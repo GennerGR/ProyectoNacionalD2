6b4a6bc25ac4309203c14d8b959576b911e9f49e
Genner <lopezitzepcgenneralexander@gmail.com>
2024-09-16 15:43:53 -0600

    Cambiando de Box a Row y refactorizando codigo

--- composeApp/src/commonMain/kotlin/com/gnr/proyectod2/addRecipes/AddRecipes.kt
+++ composeApp/src/commonMain/kotlin/com/gnr/proyectod2/addRecipes/AddRecipes.kt
@@ -1,8 +1,10 @@
 package com.gnr.proyectod2.addRecipes
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -11,7 +13,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.TestModifierUpdaterLayout
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
@@ -25,15 +26,17 @@ class AddRecipes : Screen {
 
         val navigator = LocalNavigator.currentOrThrow
         Column(modifier = Modifier.fillMaxSize()) {
-            Box(
-                modifier = Modifier.fillMaxWidth().height(55.dp).background(color = Color.Magenta)
+            Row(
+                modifier = Modifier.fillMaxWidth().height(55.dp).background(color = Color.Magenta), horizontalArrangement = Arrangement.Center
             ) {
                 IconButton(onClick = {
                     navigator.pop()
                 }) {
                     Text("Atras")
                 }
+                Spacer(modifier = Modifier.weight(1f))
                 Text("AGREGAR RECETAS", fontSize = 22.sp, color = Color.Black)
+                Spacer(modifier = Modifier.weight(1f))
             }
         }
     }
